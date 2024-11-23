02e89e987b4fd8f07ccd8734a11f0015f538405b
akanshavrma <134785932+akansha2222@users.noreply.github.com>
2024-11-23 19:47:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,9 @@
 document.getelementbyId(button).eventListener("click",function()
 		{
 			const name=document.getElementbyId(name);
-			const year=document.getElementbyId(year);							  
-		})
+			const year=document.getElementbyId(year);	
+			const url=document.getElementbyId(url)
+		const queryString = `?name=${name}&year=${year}`;
+      const url = `https://localhost:8080/${queryString}`;
+      url.innerText = url;
+    });})
