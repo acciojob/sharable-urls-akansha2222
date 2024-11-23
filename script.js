2cdadd9ecd9a9383e0813f00433e86945fe94505
akanshavrma <134785932+akansha2222@users.noreply.github.com>
2024-11-23 19:41:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // your code here
+document.getelementbyId(button).eventListener("click",function()
+		{
+			const name=document.getElementbyId(name);
+			const year=document.getElementbyId(year);							  
+		})
